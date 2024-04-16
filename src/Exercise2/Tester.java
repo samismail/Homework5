84a40f5dd1074aa682053db8a8e4c5c201da5a67
Sam <sismail1@ggc.edu>
2024-04-15 23:55:47 -0400

    inital push

--- src/Exercise2/Tester.java
+++ src/Exercise2/Tester.java
@@ -7,8 +7,10 @@ public class Tester {
     public static void main(String[] args) {
 
         Poem poem1 = new Poem("Future\n","Trynna find right in my wrongs hope my legacy lives on.\n");
-        Poem poem2 = new Poem("Eleanor Roosevelt\n","Do one thing every day that scares you.\n");
-        Poem poem3 = new Poem("Franklin D. Roosevelt\n","The only thing we have to fear is fear itself.\n");
+        Poem poem2 = new Poem("NoCap\n","It's so much of pain in us, always feel like I'm givin' up \n" +
+                                "It ain't the same no more, death brought me anger\n");
+        Poem poem3 = new Poem("Duvy\n","If they could kill me and you could stop it, would you let 'em?\n" +
+                                "It's hard to explain my feelings, this life mean more than letters");
         try(BufferedWriter br = new BufferedWriter(new FileWriter("poem1.txt"))){
             br.write(poem1.getName() + poem1.getPoet()+ poem2.getName() + poem2.getPoet()+poem3.getName() + poem3.getPoet());
 
